3e65247284ce53762e032a171ec4d09db8741528
Muhammad Aji Muharrom <ajim1417@gmail.com>
2018-07-03 20:53:29 +0700

    Menambahkan Modal Konfirmasi sebelum Verifikasi/Tolak.

--- application/views/verifikasi_main.php
+++ application/views/verifikasi_main.php
@@ -61,11 +61,11 @@
 
             // TODO: styling!
             // semua yang muncul di sini harusnya $entry['verified'] === 0
-            echo "<td>";
+            echo "<td class='action_cell'>";
 
             if($entry['verified'] == 0) {
-              echo "<a href='" . base_url("verifikasi/acc/{$entry["id"]}") . "' class='btn btn-primary'>Verifikasi</a>";
-              echo "<a href='" . base_url("verifikasi/tolak/{$entry["id"]}") . "' class='btn btn-danger'>Tolak Verifikasi</a>";
+              echo "<a href='#' onclick=\"verify_confirmation('" . base_url("verifikasi/acc/{$entry["id"]}") . "', this)\" class='btn btn-primary' data-toggle='modal' data-target='#modalKonfirmasiVerifikasi'>Verifikasi</a>";
+              echo "<a href='#' onclick=\"tolak_confirmation('" . base_url("verifikasi/tolak/{$entry["id"]}") . "', this)\" class='btn btn-danger' data-toggle='modal' data-target='#modalKonfirmasiTolak'>Tolak Verifikasi</a>";
             }
             else {
               if ($entry['verified'] > 0) echo "Terverifikasi pada ";
@@ -80,3 +80,84 @@
           ?>
         </table>
       </div>
+
+      <!-- Modal -->
+      <div class="modal fade" id="modalKonfirmasiVerifikasi" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h3 class="modal-title" >Konfirmasi Verifikasi</h3>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <a type="button" class="btn btn-secondary" data-dismiss="modal">Batal</a>
+              <a type="button" class="btn btn-primary verifikasi">Verifikasi</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="modal fade" id="modalKonfirmasiTolak" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h3 class="modal-title" id="exampleModalLabel">Konfirmasi Tolak Verifikasi</h3>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <a type="button" class="btn btn-secondary" data-dismiss="modal">Batal</a>
+              <a type="button" class="btn btn-danger verifikasi">Tolak Verifikasi</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <script type="text/javascript">
+
+      function generate_body(elem) {
+        var konten = [], heading = [];
+        $(elem).closest('tr').find('td').not('.action_cell').each(function() {
+          konten.push( $(this).html() );
+        });
+        $(elem).closest('table').find('th').not('.action_cell').each(function() {
+          heading.push( $(this).html() );
+        })
+
+        var ret = "<p>";
+        for(var i = 0;i < konten.length;++i) {
+          ret += "<b>" + heading[i] + ": </b>" + konten[i] + "<br/>";
+        }
+        ret += "</p>";
+
+        return ret;
+      }
+
+      function verify_confirmation(url, elem) {
+
+        $("#modalKonfirmasiVerifikasi .modal-body").html(
+          "<p>Yakin akan memverifikasi entri ini? </p><br/>\n" +
+          generate_body(elem)
+        );
+
+        $("#modalKonfirmasiVerifikasi .verifikasi").attr("href", url);
+      }
+
+      function tolak_confirmation(url, elem) {
+        $("#modalKonfirmasiTolak .modal-body").html(
+          "<p>Yakin akan <b>menolak verifikasi</b> entri ini? </p><br/>\n" +
+          generate_body(elem)
+        );
+
+        $("#modalKonfirmasiTolak .verifikasi").attr("href", url);
+      }
+      </script>
