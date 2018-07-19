4da6948422bfe10aa4999538d1f59ffefb2b0dbc
kevinyonathan168 <kevinyonathan168@gmail.com>
2018-07-19 11:28:31 +0700

    Fitur refleksi (via google form) - need link
    Belum ada link kesini

---
+++ application/views/refleksi.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="<?=base_url()?>assets/images/codeigniter_logo.png" type="image/x-icon">
+
+    <title>Sekolah OnkoGin - Login</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="<?=base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="<?=base_url()?>assets/css/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="<?=base_url()?>assets/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="<?=base_url()?>assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+
+</head>
+
+<body>
+<center>           
+
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfmk2eeHk5Stv4gNg0y2xiZ0JHJCJV0kleajRj19Q_ajbUcRw/viewform?embedded=true" width="100%" height="720" frameborder="1" marginheight="0" marginwidth="0">Loading...</iframe>
+	
+
+
+    <!-- jQuery -->
+    <script src="<?=base_url()?>assets/js/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="<?=base_url()?>assets/js/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="<?=base_url()?>assets/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
