546527d34d282f5e08270196c2428e83c3e13e89
kevinyonathan168 <kevinyonathan168@gmail.com>
2018-11-17 13:01:18 +0700

    Overhaul front page

--- application/views/login.php
+++ application/views/login.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="shortcut icon" href="<?=base_url()?>assets/images/codeigniter_logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="<?=base_url()?>assets/images/FKUI2.png" type="image/x-icon">
 
     <title>Sekolah OnkoGin - Login</title>
 
@@ -30,68 +30,57 @@
 <body>
 <center>           
 
-<section class="py-5" style="background:url('assets/images/grey.jpeg'); background-size:cover; color:black; border-top:2px SOLID "black">
-
-        <div class="container-fluid">
-            <div class="row">
-            <div class="col-sm-3"> <img src="assets/images/FKUI.png" width=100%> </div>
-            <div class="col-sm-6"> <center>
-                <h1 class="league-spartan">Sekolah Spesialis 2 Onkologi Ginekologi Departemen Obstetri dan Ginekologi RSCM</h1></center></div>
-            <div class="col-sm-3"> <img src="assets/images/RSCM.png" width=100%> </div>
-           </table>
-     	</div>
-
-</section>
-
-
-
-<section class="py-5" style="background:url('assets/images/grey.jpeg'); background-size:cover; color:black; border-top:2px SOLID "black">
-
-        <div class="container give_margin_top">
-            <br>
-            <p style="font-weight:bold"><strong>CARA PEMAKAIAN</strong></p>
-            <p align=center style="font-size:0.9em">Pada website ini, anda dapat menuliskan portofolio dan logbook hasil pencapaian klinik anda secara online.</p>
+  <div class="limiter">
+    <div class="container-login100">
+      <div class="wrap-login100">
+        <div class="login100-form-title" style="background-image: url(assets/images/IMERI.jpeg);">
+          <span class="login100-form-title-1">
+            Divisi Onkologi Ginekologi Departemen Obsgyn RSCM-FKUI
+          </span>
         </div>
-</section>
-<section class="py-5" style="background:url('assets/images/grey.jpeg'); background-size:cover; color:black; border-top:2px SOLID "black"><div class="container give_margin_top"> </div></section>
 
-<div class="container">
-	    <div class="row">
-	        <div class="col-md-4 col-md-offset-4">
-	            <div class="login-panel panel panel-default">
-	                <div class="panel-heading">
-	                    <h3 class="panel-title">Silakan Sign In</h3>
-	                </div>
-	                <div class="panel-body">
-	                	<small id="login-empty-input" class="error">email or password cannot be empty <br>&nbsp;</small>
-	                	<?php if($alert): ?>
-	                		<small id="login-invalid-input" class="error">invalid email or password<br>&nbsp;</small>
-	                	<?php endif; ?>
+        <form class="login100-form validate-form">
+          <div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
+            <span class="label-input100">Username</span>
+            <input class="form-control" id="email" placeholder="E-mail" name="email" type="email" autofocus>
+            <span class="focus-input100"></span>
+          </div>
 
-	                    <form role="form" method="post" onsubmit="return checkEmptyInput();" action="<?=base_url()?>authentication/login/">
-	                        <fieldset>
-	                            <div class="form-group">
-	                                <input class="form-control" id="email" placeholder="E-mail" name="email" type="email" autofocus>
-	                            </div>
-	                            <div class="form-group">
-	                                <input class="form-control" id="password" placeholder="Password" name="password" type="password" value="">
-	                            </div>
-	                            <div class="form-group">
-	                                <small><a href="#" onclick="alert('Please contact the administrator to reset your password!')">Forgot Password?</a></small>
-	                            </div>
-	                            <input id="login-submit" type="submit" value="Login" class="btn btn-lg btn-success btn-block">
-	                        </fieldset>
-	                    </form>
-	                </div>
-	            </div>
-	        </div>
-	    </div>
-	</div>
+          <div class="wrap-input100 validate-input m-b-18" data-validate = "Password is required">
+            <span class="label-input100">Password</span>
+            <input class="form-control" id="password" placeholder="Password" name="password" type="password" value="">
+            <span class="focus-input100"></span>
+          </div>
+
+          <div class="flex-sb-m w-full p-b-30">
+            <div class="contact100-form-checkbox">
+              <br>
+              <!-- BELUM JADI FUNCTIONNYA, MASIH DIPIKIRIN YANG PENTING ADA DULU-->
+              <label class="label-checkbox100" for="ckb1">
+                Remember me
+              </label>
+            </div>
+
+          <br>
+            
+            <a href="#" onclick="alert('Please contact the administrator to reset your password!')"> Forgot Password? </a><br><br>
+
+          </div>
+
+          <div class="container-login100-form-btn">
+            <input id="login-submit" type="submit" value="Login" class="btn btn-lg btn-success btn-block">
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+
 
 </center>
 
 <!-- Footer -->
-<section class="py-5" style="background:url('assets/images/grey.jpeg'); background-size:cover; color:black; border-top:2px SOLID "black">
+<section class="py-5" style="background-color:white"; background-size:cover; color:black; border-top:2px SOLID "black">
         <div class="container give_margin_top">
     <footer class="bg-dark">
       <div class="container text-white">
@@ -99,14 +88,14 @@
           <div class="col-sm-12">
             <h4 class="m-0" style="border-bottom:1px solid #ccc; padding-bottom:5px">For Question and Assistance</h4>
             <div class="row" style="padding-top:1px">
-              <div class="col-sm-6"><a href="https://api.whatsapp.com/send?phone=6282229248611&teks=Halo+boleh+bantu+soal+website+onkogyn"> <img src="assets/images/phone.png" width="20" height="20"> Aji Muharrom </a> </div>
-              <div class="col-sm-6"><a href="https://api.whatsapp.com/send?phone=6285860337688&teks=Halo+boleh+bantu+soal+website+onkogyn"><img src="assets/images/phone.png" width="20" height="20"> Kevin Yonathan</div>
+              <div class="col-sm-6"> <a href="https://api.whatsapp.com/send?phone=6282229248611&teks=Halo+boleh+bantu+soal+website+onkogyn"> <img src="assets/images/phone.png" width="20" height="20"> Aji Muharrom </a> </div>
+              <div class="col-sm-6"> <a href="https://api.whatsapp.com/send?phone=6285860337688&teks=Halo+boleh+bantu+soal+website+onkogyn"> <img src="assets/images/phone.png" width="20" height="20"> Kevin Yonathan </a> </div>
             </div>
           </div>
 
           <div class="col-sm-12">
             <br>
-            <p class="m-0 text-center text-white" style="text-align:right"><strong>Sekolah Sp2 Onkologi Ginekologi Dept Obsgin RSCM</strong></p>
+            <p class="m-0 text-center text-white" style="text-align:right"><strong>Sekolah Spesialis 2 Onkologi Ginekologi Dept Obsgin RSCM-FKUI</strong></p>
 
           </div>
         </div>
