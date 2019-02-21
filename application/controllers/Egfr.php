7b81118689835c683ca54804803531972ae0b987
kevinyonathan168 <kevinyonathan168@gmail.com>
2019-02-21 07:55:39 +0700

    Test Rumus

---
+++ application/controllers/Egfr.php
@@ -0,0 +1,23 @@
+<?php
+
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+class egfr extends CI_Controller {
+
+    public function __Construct() {
+        parent::__Construct();
+        if(!$this->session->userdata('logged_in')) {
+            redirect(base_url());
+        }
+    }
+    public function index() {
+        $this->load->view('egfr');
+    }
+
+
+}
+
+/* End of file */
